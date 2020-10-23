4aff22edaa62984163178a114673ccb29f6c820c
Katkhade Dattatraya Jankiram <67061292+knight-furry@users.noreply.github.com>
2020-10-23 13:03:00 +0530

    Create First_n_char.c

---
+++ First_n_char.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+	char name[100];
+	int count=0,i=0,n,flag=0;
+	printf("Enter a string:\n");
+	fgets(name,sizeof(name),stdin);
+	printf("enter no of alphabates:");
+	scanf("%d",&n);
+	
+	while(name[i]!='\0')
+	{
+		printf("%c",name[i]);
+		i++;
+		if(i==n)
+		break;
+	}
+}
