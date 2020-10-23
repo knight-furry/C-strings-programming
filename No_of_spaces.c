f7d1e079d57f02e987a64cd67928bc7877121fbf
Katkhade Dattatraya Jankiram <67061292+knight-furry@users.noreply.github.com>
2020-10-23 12:37:28 +0530

    Create No_of_spaces.c

---
+++ No_of_spaces.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	char name[100];
+	int count=0;
+	fgets(name,sizeof(name),stdin);
+	puts(name);
+
+	for(int i=0;name[i]!='\0';i++)
+	{
+		if(name[i]==' ')
+		count++;
+	}
+	printf("no. of spaces are:%d\n",count);
+}
